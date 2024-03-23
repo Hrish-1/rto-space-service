f7411741a9063004897733e2d9fec4976f1185ea
vignesh paulraj <vignesh.paulraj@mfscapitalpartners.com>
2024-03-23 21:22:14 +0530

    transaction details entry

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use(express.json()); // JSON Middleware
 
 connectDB()
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import  authRoutes from './routes/auth.js';
+import transactionRoutes from './routes/transactionRoutes.js';
 
 // Serve static files from "uploads" directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
