488ec4a9e024a7cb169bd4c03d79e61f009a664f
Hrish-1 <61377224+Hrish-1@users.noreply.github.com>
2024-04-03 02:37:18 +0530

    clean up environment variables, added docker-compose (#3)

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 import express from 'express'
-const app = express()
 import bodyParser from 'body-parser';
 import cors from 'cors'; // from cors
-import dotenv from 'dotenv';
 import path from 'path';
-import { fileURLToPath } from 'url';dotenv.config();
-const port = process.env.PORT || 3000
+import { fileURLToPath } from 'url';
 import connectDB from './config/db.js';
+import  authRoutes from './routes/auth.js';
+import transactionRoutes from './routes/transactionRoutes.js';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
+
+const app = express()
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json()); // JSON Middleware
+
 connectDB()
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // Serve static files from "uploads" directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
-import  authRoutes from './routes/auth.js';
-import transactionRoutes from './routes/transactionRoutes.js';
-
 app.use('/api', authRoutes); 
 app.use('/api/transaction', transactionRoutes);
 
-app.listen(port, () => {
-  console.log(`RTO app listening on port ${port}`)
-})
+const PORT = process.env.PORT || 8080 
+app.listen(PORT, () => console.log(`RTO app listening on port ${PORT}`))
