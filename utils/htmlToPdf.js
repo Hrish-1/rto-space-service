77bd1d7caee234535b2373f6fb3df195b7ce7a89
Hrish-1 <hrishikeshm997@gmail.com>
2024-05-13 14:20:39 +0530

    Refactors generate pdf method

--- utils/htmlToPdf.js
+++ utils/htmlToPdf.js
@@ -1,21 +1,7 @@
 import puppeteer from "puppeteer";
 
 export const generatePdf = async (file, options) => {
-  const finalHtml = encodeURIComponent(file.content);
-
-  const browser = await puppeteer.launch({
-    args: ["--no-sandbox"],
-    headless: true,
-  });
-
-  const page = await browser.newPage();
-  await page.goto(`data:text/html;charset=UTF-8,${finalHtml}`, {
-    waitUntil: "networkidle0",
-  });
-
-  const opts = { ...options, path: file.path }
-  await page.pdf(opts);
-  await browser.close();
+  await generatePdfs([file], options)
 };
 
 export const generatePdfs = async (files, options) => {
